2cec7418b2eabf7912aa8cf63926b7b8e96039d4
canderson1969 <71060193+canderson1969@users.noreply.github.com>
2022-09-04 21:01:24 -0700

    Quality of Life Improvements and Error Fixes
    Fixed error when using function without highlighted selection
    
    CAPS_LOCK toggles to original state after use of function, (e.g. if CAPS_LOCK was toggled before function, it will remain toggled after using the key to use the function)

--- caps.cpp
+++ caps.cpp
@@ -10,41 +10,59 @@ using namespace std;
 string caps(string s, string s2);
 string get_string(string s);
 void send_string(string s);
-void caps_press();
+void caps_press(string s);
 void copy_press();
+void emptyclipboard();
 
 int main()
 {
 	string s, s2;
-	cout << "Welcome to Caleb Anderson's CAPS_LOCK function!\n" << endl;
+	cout << "Caleb Anderson's CAPS_LOCK function...\n" << endl;
 	cout << "\nThe amount of uppercase and lowercase letters determines which case the selection will be.\n" << endl;
 	cout << "If the selection is entirely uppercase or entirely lowercase, it will switch the case of that selection entirely.\n" << endl;
-	cout << "\nJust press CAPS_LOCK on any selection of text in any text editor to begin, and press SCROLL_LOCK + CAPS_LOCK to end the program . . ." << endl;
-	while (GetKeyState(VK_SCROLL) == 0)
+	cout << "\nPress CTRL+CAPS_LOCK on any selection of text in any text editor to begin..." << endl;
+	while (true)
 	{
-		keybd_event(VK_CAPITAL, 0x3a, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
-		copy_press();
-		s = get_string(s);
-		send_string(caps(s, s2).c_str());
-		caps_press();
+		if ((GetAsyncKeyState(VK_CONTROL) && GetAsyncKeyState(VK_CAPITAL)) != 0)
+		{
+			copy_press();
+			s = get_string(s);
+			send_string(caps(s, s2).c_str());
+			caps_press(s);
+			emptyclipboard();
+			if (s != "")
+			{
+				keybd_event(VK_CAPITAL, 0x3a, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+				keybd_event(VK_CAPITAL, 0x3a, KEYEVENTF_EXTENDEDKEY, 0);
+				// these two lines turn off caps lock after the function is used to avoid frusturation
+			}
+			s = "";
+		}
+		GetAsyncKeyState(VK_CAPITAL);
+		GetAsyncKeyState(VK_CONTROL);
 	}
 	return 0;
 }
 
-string get_string(string s) // too little knowledge, all i know is that it sends the most recent entry on the clipboard to a string
+string get_string(string s) // sends the most recent entry on the clipboard to a string
 {
-	if (OpenClipboard(NULL))
+	if (IsClipboardFormatAvailable(CF_TEXT))
 	{
-		HANDLE clip;
-		clip = GetClipboardData(CF_TEXT);
-		s = (LPSTR)GlobalLock(clip);
-		GlobalUnlock(clip);
-		CloseClipboard();
+		if (OpenClipboard(NULL))
+		{
+			HANDLE clip;
+			clip = GetClipboardData(CF_TEXT);
+			s = (LPSTR)GlobalLock(clip);
+			GlobalUnlock(clip);
+			CloseClipboard();
+		}
+		else
+			return s = "";
 	}
 	return s;
 }
 
-void send_string(string s) // i can't tell what this does line by line, but it sends the modified string to the clipboard
+void send_string(string s) // sends the modified string to the clipboard
 {
 	const char* output = s.c_str();
 	const size_t len = strlen(output) + 1;
@@ -58,19 +76,23 @@ void send_string(string s) // i can't tell what this does line by line, but it s
 }
 
 
-void caps_press()
+void emptyclipboard() // empties the clipboard
 {
-	while (GetKeyState(VK_CAPITAL) == 0) // while caps_lock isn't activated, loop
+	if (OpenClipboard(NULL))
 	{
-		continue;
+		EmptyClipboard();
+		CloseClipboard();
 	}
-	if (GetKeyState(VK_CAPITAL)) // when caps_lock is activated, paste the text on the clipboard to wherever the current text editor is
-	{
+}
+
+void caps_press(string s)
+{
 		INPUT ip;
 		ip.type = INPUT_KEYBOARD;
 		ip.ki.wScan = 0;
 		ip.ki.time = 0;
 		ip.ki.dwExtraInfo = 0;
+
 		// press ctrl key
 		ip.ki.wVk = VK_CONTROL;
 		ip.ki.dwFlags = 0; // 0 for key press
@@ -81,6 +103,8 @@ void caps_press()
 		ip.ki.dwFlags = 0; // 0 for key press
 		SendInput(1, &ip, sizeof(INPUT));
 
+		Sleep(10);
+
 		// release v
 		ip.ki.wVk = 'V';
 		ip.ki.dwFlags = KEYEVENTF_KEYUP;
@@ -90,35 +114,33 @@ void caps_press()
 		ip.ki.wVk = VK_CONTROL;
 		ip.ki.dwFlags = KEYEVENTF_KEYUP;
 		SendInput(1, &ip, sizeof(INPUT));
-	}	
-	keybd_event(VK_CAPITAL, 0x3a, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
-	keybd_event(VK_CAPITAL, 0x3a, KEYEVENTF_EXTENDEDKEY, 0);		// these two lines turn off caps lock after the function is used to avoid frusturation
+
+		
+
 }
 
 void copy_press()
 {
-	while (GetKeyState(VK_CAPITAL) == 0) // while caps_lock isn't activated, loop
-	{
-		continue;
-	}
-	if (GetKeyState(VK_CAPITAL)) // when caps_lock is activated, do ctrl+c
-	{
+	
 		INPUT ip;
 		ip.type = INPUT_KEYBOARD;
 		ip.ki.wScan = 0;
 		ip.ki.time = 0;
 		ip.ki.dwExtraInfo = 0;
+
 		// press ctrl key
 		ip.ki.wVk = VK_CONTROL;
 		ip.ki.dwFlags = 0; // 0 for key press
 		SendInput(1, &ip, sizeof(INPUT));
 
-		// Press v key
+		// Press c key
 		ip.ki.wVk = 'C';
 		ip.ki.dwFlags = 0; // 0 for key press
 		SendInput(1, &ip, sizeof(INPUT));
+		
+		Sleep(10);
 
-		// release v
+		// release c
 		ip.ki.wVk = 'C';
 		ip.ki.dwFlags = KEYEVENTF_KEYUP;
 		SendInput(1, &ip, sizeof(INPUT));
@@ -127,104 +149,100 @@ void copy_press()
 		ip.ki.wVk = VK_CONTROL;
 		ip.ki.dwFlags = KEYEVENTF_KEYUP;
 		SendInput(1, &ip, sizeof(INPUT));
-	}
-	Sleep(100);
-	
 }
 
 
 string caps(string s, string s2)
+{
+	int upper = 0;
+	int lower = 0;
+	int len = s.length();
+	for (int p = 0; p < len; p++)
 	{
-		int upper = 0;
-		int lower = 0;
-		int len = s.length();
-		for (int p = 0; p < len; p++)
-		{
-			if (isupper(s[p]))
-				upper++;
-			if (islower(s[p]))
-				lower++;
-		}
-
-		if ((upper > lower || upper == lower) && lower != 0) 
-		{
-			for (int i = 0; i < len; i++)
-			{
-				if ((s[i] > 123 || s[i] < 65) || (s[i] > 90 && s[i] < 97))
-				{
-					s2.push_back(s[i]);
-					continue;
-				}
-				if (islower(s[i]))
-				{
-					char c = toupper(s[i]);
-					s2.push_back(c);
-					continue;
-				}
-				if (isupper(s[i]))
-					s2.push_back(s[i]);				
-			}
-		}
-
-		if (upper == 0)
-		{
-			for (int i = 0; i < len; i++)
-			{
-				if ((s[i] > 123 || s[i] < 65) || (s[i] > 90 && s[i] < 97))
-				{
-					s2.push_back(s[i]);
-					continue;
-				}
-				if (islower(s[i]))
-				{
-					char c = toupper(s[i]);
-					s2.push_back(c);
-					continue;
-				}
-				if (isupper(s[i]))
-					s2.push_back(s[i]);
-			}
-		}
-
-		if (lower == 0)
-		{
-			for (int i = 0; i < len; i++)
-			{
-				if ((s[i] > 123 || s[i] < 65) || (s[i] > 90 && s[i] < 97))
-				{
-					s2.push_back(s[i]);
-					continue;
-				}
-				if (isupper(s[i]))
-				{
-					char c = tolower(s[i]);
-					s2.push_back(c);
-					continue;
-				}
-				if (islower(s[i]))
-					s2.push_back(s[i]);
-			}
-		}
-
-		if ((lower > upper) && upper != 0)
-		{
-			for (int i = 0; i < len; i++)
-			{
-				if ((s[i] > 123 || s[i] < 65) || (s[i] > 90 && s[i] < 97))
-				{
-					s2.push_back(s[i]);
-					continue;
-				}
-				if (isupper(s[i]))
-				{
-					char c = tolower(s[i]);
-					s2.push_back(c);
-					continue;
-				}
-				if (islower(s[i]))
-					s2.push_back(s[i]);
-			}
-		}
-	return s2;
+		if (isupper(s[p]))
+			upper++;
+		if (islower(s[p]))
+			lower++;
 	}
 
+	if ((upper > lower || upper == lower) && lower != 0)
+	{
+		for (int i = 0; i < len; i++)
+		{
+			if ((s[i] > 123 || s[i] < 65) || (s[i] > 90 && s[i] < 97))
+			{
+				s2.push_back(s[i]);
+				continue;
+			}
+			if (islower(s[i]))
+			{
+				char c = toupper(s[i]);
+				s2.push_back(c);
+				continue;
+			}
+			if (isupper(s[i]))
+				s2.push_back(s[i]);
+		}
+	}
+
+	if (upper == 0)
+	{
+		for (int i = 0; i < len; i++)
+		{
+			if ((s[i] > 123 || s[i] < 65) || (s[i] > 90 && s[i] < 97))
+			{
+				s2.push_back(s[i]);
+				continue;
+			}
+			if (islower(s[i]))
+			{
+				char c = toupper(s[i]);
+				s2.push_back(c);
+				continue;
+			}
+			if (isupper(s[i]))
+				s2.push_back(s[i]);
+		}
+	}
+
+	if (lower == 0)
+	{
+		for (int i = 0; i < len; i++)
+		{
+			if ((s[i] > 123 || s[i] < 65) || (s[i] > 90 && s[i] < 97))
+			{
+				s2.push_back(s[i]);
+				continue;
+			}
+			if (isupper(s[i]))
+			{
+				char c = tolower(s[i]);
+				s2.push_back(c);
+				continue;
+			}
+			if (islower(s[i]))
+				s2.push_back(s[i]);
+		}
+	}
+
+	if ((lower > upper) && upper != 0)
+	{
+		for (int i = 0; i < len; i++)
+		{
+			if ((s[i] > 123 || s[i] < 65) || (s[i] > 90 && s[i] < 97))
+			{
+				s2.push_back(s[i]);
+				continue;
+			}
+			if (isupper(s[i]))
+			{
+				char c = tolower(s[i]);
+				s2.push_back(c);
+				continue;
+			}
+			if (islower(s[i]))
+				s2.push_back(s[i]);
+		}
+	}
+	return s2;
+}
